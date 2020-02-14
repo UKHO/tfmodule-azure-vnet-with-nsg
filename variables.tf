dd479dbc4b72fe906f199b286c2821175adcbcfc
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2020-02-14 14:15:33 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,22 @@
-variable "name" {
-  type = string
-  default = "test"
+variable "prefix" {  
 }
 
+variable "resource_group_name" {
+}
+
+variable "address" {  
+}
+
+variable "subnets" {    
+}
+
+variable "newbits" {
+  default = 4
+}
+
+variable "service_endpoints" {  
+}
+
+variable "tags" {
+  type = map
+}
