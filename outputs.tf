f422ea6e4ff1dff47d9338f614c2a39a0ae7ea55
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2020-02-18 09:48:51 +0000

    Removing rg outputs from module (#5)
    * Remove RG from outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "resource_group_name" {
-  value = "${azurerm_resource_group.rg.name}"
-}
-
-output "location" {
-  value = "${azurerm_resource_group.rg.location}"
-}
-
 output "network_security_group_name" {
   value = "${azurerm_network_security_group.nsg.name}"
 }
