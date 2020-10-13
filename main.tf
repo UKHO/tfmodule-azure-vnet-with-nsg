a7567204ff8d41afb91a8b6942490a0b0c920305
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2020-10-13 12:18:25 +0100

    Set provider

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
+
 locals {
   base_cidr_block = var.address
 }
 
+provider "azurerm" {
+  alias = "src"
+}
+
+
 resource "azurerm_network_security_group" "nsg" {
+  provider            = azurerm.src
   name                = "${var.prefix}-nsg"
   location            = var.resource_group.location
   resource_group_name = var.resource_group.name
@@ -13,6 +20,7 @@ resource "azurerm_network_security_group" "nsg" {
 
 resource "azurerm_virtual_network" "spokevnet" {
   name                = "${var.prefix}-vnet"
+  provider            = azurerm.src
   address_space       = [local.base_cidr_block]
   location            = var.resource_group.location
   resource_group_name = var.resource_group.name
