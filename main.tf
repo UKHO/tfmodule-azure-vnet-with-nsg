89faedbd9d0a56916a041bdb216d9b89d5021be0
Tom Barham 🦄 <24593464+ecstaticrainbow@users.noreply.github.com>
2021-02-02 10:39:51 +0000

    fixing the resource group name from bad copy paste Co-authored-by: JZ <32240008+jarekzaj@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_subnet" "spokesubnet" {
   count                = length(var.subnets)
   name                 = var.subnets[count.index].name
   provider             = azurerm.src
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = var.resource_group.name
   virtual_network_name = azurerm_virtual_network.spokevnet.name
   address_prefixes     = [cidrsubnet(local.base_cidr_block, var.newbits, var.subnets[count.index].number)]
   service_endpoints    = var.service_endpoints
