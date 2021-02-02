af795ccde9372f97d134cc1316b1e1f90510561a
Tom Barham <tjbarham@btinternet.com>
2021-02-02 12:04:16 +0000

    Add service endpoints to the subnet (#10)
    * adding the service endpoints to the subnet
    
    * changing up how the subnets are added to allow service endpoints to be added
    
    * adding optional delegation
    
    * fixed the nsg association and made delegation optional properly
    
    Co-authored-by: JZ <32240008+jarekzaj@users.noreply.github.com>
    
    * fixing the resource group name from bad copy paste
    Co-authored-by: JZ <32240008+jarekzaj@users.noreply.github.com>
    
    * adding a default action so that azure doesn't keep triyng to clear its default
    
    Co-authored-by: JZ <32240008+jarekzaj@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -28,17 +28,32 @@ resource "azurerm_virtual_network" "spokevnet" {
   tags                = var.tags
   lifecycle { ignore_changes = [tags] }
 
+}
 
-  dynamic "subnet" {
-    for_each = [for s in var.subnets : {
-      name   = s.name
-      prefix = cidrsubnet("${local.base_cidr_block}", var.newbits, s.number)
-    }]
+resource "azurerm_subnet" "spokesubnet" {
+  count                = length(var.subnets)
+  name                 = var.subnets[count.index].name
+  provider             = azurerm.src
+  resource_group_name  = var.resource_group.name
+  virtual_network_name = azurerm_virtual_network.spokevnet.name
+  address_prefixes     = [cidrsubnet(local.base_cidr_block, var.newbits, var.subnets[count.index].number)]
+  service_endpoints    = var.service_endpoints
 
+  dynamic "delegation" {
+    for_each = try(var.subnets[count.index].delegation, null) == null ? [] : [1]
     content {
-      name           = subnet.value.name
-      address_prefix = subnet.value.prefix
-      security_group = azurerm_network_security_group.nsg.id
+      name = "delegation"
+
+      service_delegation {
+        name    = var.subnets[count.index].delegation.name
+        actions = try(var.subnets[count.index].delegation.actions, ["Microsoft.Network/virtualNetworks/subnets/action"])
+      }
     }
   }
+}
+
+resource "azurerm_subnet_network_security_group_association" "spokesubnetnsg" {
+  count                     = length(var.subnets)
+  subnet_id                 = azurerm_subnet.spokesubnet[count.index].id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "network_security_group_name" {
-  value = "${azurerm_network_security_group.nsg.name}"
+  value = azurerm_network_security_group.nsg.name
 }
