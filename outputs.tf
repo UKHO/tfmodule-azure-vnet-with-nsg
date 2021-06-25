ff604017be78bfec14bb402574f0fcd799d35dbc
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2021-06-25 15:26:17 +0100

    Adding NSG ID and VNET name to module output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+output "virtual_network_name" {
+  value = azurerm_virtual_network.spokevnet.name
+}
+
+output "network_security_group_id" {
+  value = azurerm_network_security_group.nsg.id
+}
+
 output "network_security_group_name" {
   value = azurerm_network_security_group.nsg.name
 }
