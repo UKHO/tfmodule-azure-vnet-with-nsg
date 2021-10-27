7efe8632e9f7260cabffa4d9fab4fbe50776fbe0
Shedman <52528924+ShaunBresnahan@users.noreply.github.com>
2021-10-27 14:12:39 +0100

    Removed 443 and 53 rules, this will become part of the spoke nsg-rules

--- spoke-nsg-rules.tf
+++ spoke-nsg-rules.tf
@@ -12,18 +12,3 @@ resource "azurerm_network_security_rule" "deny" {
   resource_group_name         = var.resource_group.name
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
-
-resource "azurerm_network_security_rule" "allow" { 
-  provider                    = azurerm.src
-  name                        = "AllowInternetOutbound"
-  priority                    = 200
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_ranges      = ["53", "443"]
-  source_address_prefix       = "*"
-  destination_address_prefix  = "Internet"
-  resource_group_name         = var.resource_group.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-}
