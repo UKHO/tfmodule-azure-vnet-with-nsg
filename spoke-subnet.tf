c003ce801d2297b7914594c2a662afcff6272127
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2024-07-09 12:11:29 +0100

    Update spoke-subnet.tf

--- spoke-subnet.tf
+++ spoke-subnet.tf
@@ -6,7 +6,7 @@ resource "azurerm_subnet" "spokesubnet" {
   virtual_network_name = azurerm_virtual_network.spokevnet.name
   address_prefixes     = [cidrsubnet(local.base_cidr_block, try(var.subnets[count.index].newbits,var.newbits), var.subnets[count.index].number)]
   service_endpoints    = var.service_endpoints
-  lifecycle { ignore_changes = [delegation,private_endpoint_network_policies] }
+  lifecycle { ignore_changes = [private_endpoint_network_policies] }
 } 
 
 resource "azurerm_subnet" "spokesubnet_delegated" {
@@ -15,7 +15,7 @@ resource "azurerm_subnet" "spokesubnet_delegated" {
   provider             = azurerm.src
   resource_group_name  = var.resource_group.name
   virtual_network_name = azurerm_virtual_network.spokevnet.name
-  address_prefixes     = [cidrsubnet(local.base_cidr_block, var.newbits, var.subnets_with_delegation[count.index].number)]
+  address_prefixes     = [cidrsubnet(local.base_cidr_block, try(var.subnets[count.index].newbits,var.newbits), var.subnets_with_delegation[count.index].number)]
   service_endpoints    = var.service_endpoints
   delegation {
       name = "delegation"
@@ -25,7 +25,7 @@ resource "azurerm_subnet" "spokesubnet_delegated" {
           actions = try(var.subnets_with_delegation[count.index].delegation.actions, ["Microsoft.Network/virtualNetworks/subnets/action"])
       }
     }
-  lifecycle { ignore_changes = [enforce_private_link_endpoint_network_policies] }
+  lifecycle { ignore_changes = [private_endpoint_network_policies] }
 } 
 
 resource "azurerm_subnet_network_security_group_association" "spokesubnetnsg" {
