73b1ed3cd82ddafb54df8e4691c4267006a0500d
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2024-07-09 12:08:02 +0100

    Added resource with delegation

--- spoke-subnet.tf
+++ spoke-subnet.tf
@@ -9,6 +9,25 @@ resource "azurerm_subnet" "spokesubnet" {
   lifecycle { ignore_changes = [delegation,private_endpoint_network_policies] }
 } 
 
+resource "azurerm_subnet" "spokedelegatedsubnet" {
+  count                = length(var.subnets_with_delegation)
+  name                 = var.subnets_with_delegation[count.index].name
+  provider             = azurerm.src
+  resource_group_name  = var.resource_group.name
+  virtual_network_name = azurerm_virtual_network.spokevnet.name
+  address_prefixes     = [cidrsubnet(local.base_cidr_block, var.newbits, var.subnets_with_delegation[count.index].number)]
+  service_endpoints    = var.service_endpoints
+  delegation {
+      name = "delegation"
+  
+      service_delegation {
+        name    = var.subnets_with_delegation[count.index].delegation.name
+          actions = try(var.subnets_with_delegation[count.index].delegation.actions, ["Microsoft.Network/virtualNetworks/subnets/action"])
+      }
+    }
+  lifecycle { ignore_changes = [enforce_private_link_endpoint_network_policies] }
+} 
+
 resource "azurerm_subnet_network_security_group_association" "spokesubnetnsg" {
   provider                  = azurerm.src
   count                     = length(var.subnets)
