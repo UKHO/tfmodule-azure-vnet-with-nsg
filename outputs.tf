27808804246274db6c4280b949705526baaeffd4
Shedman <52528924+ShaunBresnahan@users.noreply.github.com>
2025-05-13 15:07:31 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "network_security_group_id" {
 output "network_security_group_name" {
   value = azurerm_network_security_group.nsg.name
 }
+
+output "virtual_network_id" {
+  value = azurerm_virtual_network.spokevnet.id  # Ensure you're referencing the resource ID
+}
