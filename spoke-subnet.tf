68f2731dd00fa4d4c949ff967fa1b6eb5851f73e
Shaun Bresnahan <ashaun@ukho.gov.uk>
2025-08-07 13:37:36 +0100

    Test new structure

--- spoke-subnet.tf
+++ spoke-subnet.tf
@@ -1,29 +1,29 @@
 resource "azurerm_subnet" "spokesubnet" {
-  count                           = length(var.subnets)
-  name                            = var.subnets[count.index].name
+  for_each                        = { for subnet in var.subnets : subnet.name => subnet }
+  name                            = each.value.name
   provider                        = azurerm.src
   resource_group_name             = var.resource_group.name
   virtual_network_name            = azurerm_virtual_network.spokevnet.name
-  address_prefixes                = [cidrsubnet(local.base_cidr_block, try(var.subnets[count.index].newbits, var.newbits), var.subnets[count.index].number)]
-  service_endpoints               = try(var.subnets[count.index].service_endpoints, var.service_endpoints)
+  address_prefixes                = [cidrsubnet(local.base_cidr_block, try(each.value.newbits, var.newbits), each.value.number)]
+  service_endpoints               = try(each.value.service_endpoints, var.service_endpoints)
   default_outbound_access_enabled = var.enable_outbound
   lifecycle { ignore_changes = [private_endpoint_network_policies] }
 }
 
 resource "azurerm_subnet" "spokesubnet_delegated" {
-  count                = length(var.subnets_with_delegation)
-  name                 = var.subnets_with_delegation[count.index].name
+  for_each             = { for subnet in var.subnets_with_delegation : subnet.name => subnet }
+  name                 = each.value.name
   provider             = azurerm.src
   resource_group_name  = var.resource_group.name
   virtual_network_name = azurerm_virtual_network.spokevnet.name
-  address_prefixes     = [cidrsubnet(local.base_cidr_block, try(var.subnets_with_delegation[count.index].newbits, var.newbits), var.subnets_with_delegation[count.index].number)]
-  service_endpoints    = try(var.subnets_with_delegation[count.index].service_endpoints, var.service_endpoints)
+  address_prefixes     = [cidrsubnet(local.base_cidr_block, try(each.value.newbits, var.newbits), each.value.number)]
+  service_endpoints    = try(each.value.service_endpoints, var.service_endpoints)
   delegation {
-    name = replace(var.subnets_with_delegation[count.index].delegation.name, "/", ".")
+    name = replace(each.value.delegation.name, "/", ".")
 
     service_delegation {
-      name    = var.subnets_with_delegation[count.index].delegation.name
-      actions = try(var.subnets_with_delegation[count.index].delegation.actions, ["Microsoft.Network/virtualNetworks/subnets/action"])
+      name    = each.value.delegation.name
+      actions = try(each.value.delegation.actions, ["Microsoft.Network/virtualNetworks/subnets/action"])
     }
   }
   default_outbound_access_enabled = var.enable_outbound
@@ -32,14 +32,14 @@ resource "azurerm_subnet" "spokesubnet_delegated" {
 
 resource "azurerm_subnet_network_security_group_association" "spokesubnetnsg" {
   provider                  = azurerm.src
-  count                     = length(var.subnets)
-  subnet_id                 = azurerm_subnet.spokesubnet[count.index].id
+  for_each                  = azurerm_subnet.spokesubnet
+  subnet_id                 = each.value.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_subnet_network_security_group_association" "spokesubnetdelegatednsg" {
   provider                  = azurerm.src
-  count                     = length(var.subnets_with_delegation)
-  subnet_id                 = azurerm_subnet.spokesubnet_delegated[count.index].id
+  for_each                  = azurerm_subnet.spokesubnet_delegated
+  subnet_id                 = each.value.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
