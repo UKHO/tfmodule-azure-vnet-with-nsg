3a0b0d4aaa27f78ac587cdb1b84a3ab10ff9ac42
Shedman <52528924+ShaunBresnahan@users.noreply.github.com>
2025-09-04 14:01:40 +0100

    updated newbits mistake (#18)
    Co-authored-by: Shaun Bresnahan <ashaun@ukho.gov.uk>

--- spoke-subnet.tf
+++ spoke-subnet.tf
@@ -4,7 +4,7 @@ resource "azurerm_subnet" "spokesubnet" {
   provider             = azurerm.src
   resource_group_name  = var.resource_group.name
   virtual_network_name = azurerm_virtual_network.spokevnet.name
-  address_prefixes     = [cidrsubnet(local.base_cidr_block, var.newbits, each.value.number)]
+  address_prefixes     = [cidrsubnet(local.base_cidr_block,try(each.value.newbits, var.newbits), each.value.number)]
   service_endpoints    = var.service_endpoints
 
   dynamic "delegation" {
