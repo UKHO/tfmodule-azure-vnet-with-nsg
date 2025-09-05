7edab98ea845cbcb83b1e5ed619af831c52a143e
Shaun Bresnahan <ashaun@ukho.gov.uk>
2025-09-05 10:06:21 +0100

    changed address prefix to use coalesce

--- spoke-subnet.tf
+++ spoke-subnet.tf
@@ -4,7 +4,7 @@ resource "azurerm_subnet" "spokesubnet" {
   provider             = azurerm.src
   resource_group_name  = var.resource_group.name
   virtual_network_name = azurerm_virtual_network.spokevnet.name
-  address_prefixes     = [cidrsubnet(local.base_cidr_block,try(each.value.newbits, var.newbits), each.value.number)]
+  address_prefixes     = [cidrsubnet(local.base_cidr_block,coalesce(each.value.newbits, var.newbits),each.value.number)]
   service_endpoints    = var.service_endpoints
 
   dynamic "delegation" {
@@ -24,4 +24,4 @@ resource "azurerm_subnet_network_security_group_association" "spokesubnetnsg" {
   for_each                  = azurerm_subnet.spokesubnet
   subnet_id                 = each.value.id
   network_security_group_id = azurerm_network_security_group.nsg.id
-}
+} 
