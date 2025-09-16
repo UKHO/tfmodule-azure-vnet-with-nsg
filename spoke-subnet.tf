7c9cb8acc7b1913fe074a2f59ae2334420aebfe5
Mark <52573122+skins-uk@users.noreply.github.com>
2025-09-16 10:38:12 +0100

    Adds in outbound access on subnet (#20)

--- spoke-subnet.tf
+++ spoke-subnet.tf
@@ -6,7 +6,8 @@ resource "azurerm_subnet" "spokesubnet" {
   virtual_network_name = azurerm_virtual_network.spokevnet.name
   address_prefixes     = [cidrsubnet(local.base_cidr_block,coalesce(each.value.newbits, var.newbits),each.value.number)]
   service_endpoints    = var.service_endpoints
-
+  default_outbound_access_enabled = var.enable_outbound
+  
   dynamic "delegation" {
     for_each = each.value.delegation_name != null ? [1] : []
     content {
