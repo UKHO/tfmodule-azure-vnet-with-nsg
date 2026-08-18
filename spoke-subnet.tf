955f73fff8d2db37ebef57bbb89a2db5034174ac
sambez-work <130447321+sambez-work@users.noreply.github.com>
2026-08-18 09:34:56 +0100

    Merge pull request #23 from UKHO/Testing-Azurrm-v5
    removing service endpoint attribute

--- spoke-subnet.tf
+++ spoke-subnet.tf
@@ -5,7 +5,6 @@ resource "azurerm_subnet" "spokesubnet" {
   resource_group_name  = var.resource_group.name
   virtual_network_name = azurerm_virtual_network.spokevnet.name
   address_prefixes     = [cidrsubnet(local.base_cidr_block,coalesce(each.value.newbits, var.newbits),each.value.number)]
-  service_endpoints    = var.service_endpoints
   default_outbound_access_enabled = var.enable_outbound
   
   dynamic "delegation" {
