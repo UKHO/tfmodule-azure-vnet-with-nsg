a1b501c57c044015b7a899bf6ce3c70b87aadb46
GhostShaikh <farhan.shaikh@ukho.gov.uk>
2026-09-14 14:40:17 +0100

    Checking the service endpoint variable for v5 in azurerm

--- spoke-subnet.tf
+++ spoke-subnet.tf
@@ -4,10 +4,10 @@ resource "azurerm_subnet" "spokesubnet" {
   provider             = azurerm.src
   resource_group_name  = var.resource_group.name
   virtual_network_name = azurerm_virtual_network.spokevnet.name
-  address_prefixes     = [cidrsubnet(local.base_cidr_block, try(var.subnets[count.index].newbits,var.newbits), var.subnets[count.index].number)]
-  service_endpoints    = try(var.subnets[count.index].service_endpoints, var.service_endpoints)
+  address_prefixes     = [cidrsubnet(local.base_cidr_block, try(var.subnets[count.index].newbits, var.newbits), var.subnets[count.index].number)]
+  service_endpoint     = try(var.subnets[count.index].service_endpoints, var.service_endpoints)
   lifecycle { ignore_changes = [private_endpoint_network_policies] }
-} 
+}
 
 resource "azurerm_subnet" "spokesubnet_delegated" {
   count                = length(var.subnets_with_delegation)
@@ -15,18 +15,18 @@ resource "azurerm_subnet" "spokesubnet_delegated" {
   provider             = azurerm.src
   resource_group_name  = var.resource_group.name
   virtual_network_name = azurerm_virtual_network.spokevnet.name
-  address_prefixes     = [cidrsubnet(local.base_cidr_block, try(var.subnets_with_delegation[count.index].newbits,var.newbits), var.subnets_with_delegation[count.index].number)]
-  service_endpoints    = try(var.subnets_with_delegation[count.index].service_endpoints,var.service_endpoints)
+  address_prefixes     = [cidrsubnet(local.base_cidr_block, try(var.subnets_with_delegation[count.index].newbits, var.newbits), var.subnets_with_delegation[count.index].number)]
+  service_endpoint     = try(var.subnets_with_delegation[count.index].service_endpoints, var.service_endpoints)
   delegation {
-      name = replace(var.subnets_with_delegation[count.index].delegation.name,"/",".")
-  
-      service_delegation {
-        name = var.subnets_with_delegation[count.index].delegation.name
-          actions = try(var.subnets_with_delegation[count.index].delegation.actions, ["Microsoft.Network/virtualNetworks/subnets/action"])
-      }
+    name = replace(var.subnets_with_delegation[count.index].delegation.name, "/", ".")
+
+    service_delegation {
+      name    = var.subnets_with_delegation[count.index].delegation.name
+      actions = try(var.subnets_with_delegation[count.index].delegation.actions, ["Microsoft.Network/virtualNetworks/subnets/action"])
     }
+  }
   lifecycle { ignore_changes = [private_endpoint_network_policies] }
-} 
+}
 
 resource "azurerm_subnet_network_security_group_association" "spokesubnetnsg" {
   provider                  = azurerm.src
