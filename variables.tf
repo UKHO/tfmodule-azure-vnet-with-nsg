ab0d9bf125c65eaa5ba73ba513b764d31ac26009
GhostShaikh <farhan.shaikh@ukho.gov.uk>
2026-09-24 11:05:20 +0100

    Update service endpoint refernce for azurerm v5

--- variables.tf
+++ variables.tf
@@ -27,7 +27,6 @@ variable "subnets" {
     number             = number
     delegation_name    = optional(string)
     delegation_actions = optional(list(string))
-    service_endpoints    = optional(list(string))
   }))
 }
 
